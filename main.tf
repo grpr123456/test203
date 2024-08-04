ab982266acda54be22d362a37f1e7bc06036b294
grpr123456 <grpr.mca21@gmail.com>
2024-08-05 04:11:39 +0530

    repo update

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "first" {
-    source = "git::https://github.com/grpr123456/test202.git"
+    source = "git::https://github.com/grpr123456/test203.git"
     vpc_cidr="10.2.0.0/16"
     subnet1_cidr="10.2.1.0/24"
     subnet2_cidr="10.2.2.0/24"
